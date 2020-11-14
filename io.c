c3a32af86162efda62d88aa9ca6f9b1ec0ce549c
Martijn Bos <maboc@maboc.nl>
2020-11-14 10:37:47 +0100

    Het (voor t eerst) schrijven van data lijkt nu aardig te werken

--- io.c
+++ io.c
@@ -96,14 +96,42 @@ int initial_load(){
 
 int write_node(struct node_type * node, struct base_type * base){
   char * tmp;
-
+  unsigned long long pos;
+  FILE * fp;
+  int t=3; //type : node (zie mem_disk.txt)
+  
   tmp=malloc(100);
   sprintf(tmp, "Write node ID : %i", node->id);
   logger(tmp);
   free(tmp);
 
+  if(node->status==2){
+    logger("really...nothing changed");
+  } else if(node->status==1){
+    logger("just created");
+    
+    pos=get_empty_spot();
+    fp=fopen("graphive.dat", "r+");
+    fseek(fp, pos, SEEK_SET);
+    
+    fwrite(&t, sizeof(int), 1, fp);
+    fwrite(& base->id, sizeof(int), 1, fp);
+    fwrite(& node->id, sizeof(unsigned long long), 1, fp);
+    fwrite(&"O", sizeof(char),1,fp);
+    
+    node->file=0;
+    node->location=pos;
+    node->status=2;
+    
+    pos=pos+sizeof(int)+sizeof(int)+sizeof(unsigned long long)+ sizeof(char);
+    
+    set_empty_spot(pos);
+    
+    fclose(fp);
+  }
+  
   write_attributes(node->attributelist, base, node);
-     
+  
   return 0;
 }
 
@@ -127,13 +155,67 @@ int write_nodes(struct dll * list, struct base_type * base){
 
 int write_attribute(struct attribute_type * attr, struct base_type * base, struct node_type * node){
   char * tmp;
+  int t; //type of the object we are going to write
+  unsigned long long pos;
+  FILE * fp;
+  int kl;
+  int vl;
+  char * null_char;
   
   tmp=malloc(1000);
-
-  sprintf(tmp, "Attribute : ID : %i Key : %s Value : %s", attr->id, attr->key, attr->val);
-  logger(tmp);
-  free(tmp);
+  null_char=malloc(1);
+  bzero(null_char, 1);
   
+  if(node==NULL){
+    sprintf(tmp, "Base Attribute : ID : %i Key : %s Value : %s", attr->id, attr->key, attr->val);
+    logger(tmp);
+    free(tmp);
+  } else {
+    sprintf(tmp, "Node Attribute : ID : %i Key : %s Value : %s", attr->id, attr->key, attr->val);
+    logger(tmp);
+    free(tmp);
+  }
+  
+  if(attr->status==2){
+    logger("nothing to write");
+  }else if (attr->status==1){
+    logger("Just created");
+    
+    pos=get_empty_spot();
+    t=2;  
+    
+    fp=fopen("graphive.dat", "r+");
+    fseek(fp, pos, SEEK_SET);
+    fwrite(&t, sizeof(int), 1, fp);//type
+    fwrite(&(base->id), sizeof(int), 1, fp); //base
+    if(node==NULL){
+      fwrite(null_char,sizeof(unsigned long long),1,fp); //node
+    }else{
+      fwrite(& node->id, sizeof(unsigned long long), 1, fp);
+    }
+    fwrite(null_char ,sizeof(unsigned long long),1,fp); //relation
+    fwrite(&(attr->id), sizeof(unsigned long long), 1, fp); // ID
+    kl=strlen(attr->key);
+    fwrite(&kl, sizeof(int),1,fp);
+    fwrite(attr->key, strlen(attr->key), 1,  fp);
+    vl=strlen(attr->val);
+    fwrite(&vl, sizeof(int), 1, fp);
+    fwrite(attr->val, strlen(attr->val), 1, fp);
+    fwrite(&"O", 1, 1, fp);
+    
+    free(null_char);
+    
+    fclose(fp);
+    
+    pos=pos+sizeof(int)+sizeof(int)+sizeof(unsigned long long)+sizeof(unsigned long long)+sizeof(unsigned long long) + sizeof(int)+ strlen(attr->key)+sizeof(int)+strlen(attr->val)+1;
+    set_empty_spot(pos);
+    
+    attr->file=0;
+    attr->location=pos;
+    attr->status=2;
+    
+  }
+    
   return 0;
 }
 
